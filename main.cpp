357d75b2235f892a9b85efac099a73c1b85fc5ba
Eyub Celebioglu <100705373+Eyub4k@users.noreply.github.com>
2025-03-28 17:30:34 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,202 @@
+#include <glad.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include "Camera.h"
+#include "Chunk.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "utils.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+const char* title = "Minecraft Clone";
+
+GLuint shaderProgram; // Déclaré globalement pour être accessible dans render
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    Camera* camera = static_cast<Camera*>(glfwGetWindowUserPointer(window));
+    camera->processMouseMovement(xpos, ypos);
+}
+
+void checkShaderCompile(GLuint shader, const char* type) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cerr << "ERREUR DE COMPILATION DU " << type << " SHADER : " << infoLog << std::endl;
+    }
+}
+
+void checkOpenGLError(const char* message) {
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR) {
+        std::cerr << "OpenGL Error (" << message << "): " << error << std::endl;
+    }
+}
+
+unsigned int loadTexture(const char* path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, (nrChannels == 4 ? GL_RGBA : GL_RGB), GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Échec du chargement de la texture : " << path << std::endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+}
+
+int main() {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, title, NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Échec du chargement des fonctions OpenGL" << std::endl;
+        return -1;
+    }
+
+    Camera camera(glm::vec3(8.0f, 15.0f, 8.0f), -90.0f, 0.0f);
+    Chunk chunk;
+
+    glfwSetWindowUserPointer(window, &camera);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    const char* vertexShaderSource = R"(
+        #version 330 core
+        layout(location = 0) in vec3 aPos;
+        layout(location = 1) in vec2 aTexCoord;
+        out vec2 TexCoord;
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
+        void main() {
+            gl_Position = projection * view * model * vec4(aPos, 1.0);
+            TexCoord = aTexCoord;
+        }
+    )";
+
+    const char* fragmentShaderSource = R"(
+        #version 330 core
+        out vec4 FragColor;
+        in vec2 TexCoord;
+        uniform sampler2D texture1;
+    
+        void main() {
+            FragColor = texture(texture1, TexCoord);
+        }
+    )";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    checkShaderCompile(vertexShader, "VERTEX");
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    checkShaderCompile(fragmentShader, "FRAGMENT");
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Vérifier le linkage du programme
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERREUR DE LINKAGE DU PROGRAMME SHADER : " << infoLog << std::endl;
+        return -1;
+    }
+
+    glUseProgram(shaderProgram);
+
+    // Charger l’atlas 64x64
+    unsigned int textureID = loadTexture("C:/Users/eyubc/OneDrive/Bureau/minecraftcpp/texture/atlas2.png");
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0); // Uniform pour l’atlas
+
+    // Activer la texture
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    chunk.initializeBuffers();
+
+    glm::mat4 model = glm::mat4(1.0f);
+    GLuint modelLoc = glGetUniformLocation(shaderProgram, "model");
+    GLuint viewLoc = glGetUniformLocation(shaderProgram, "view");
+    GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+
+    if (modelLoc == -1 || viewLoc == -1 || projectionLoc == -1) {
+        std::cerr << "Erreur : Uniforms non trouvés" << std::endl;
+        return -1;
+    }
+
+    float lastFrame = 0.0f;
+    float deltaTime = 0.0f;
+
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        glClearColor(0.5f, 0.7f, 1.0f, 1.0f); // Bleu ciel Minecraft
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        checkOpenGLError("clear");
+
+        camera.processInput(window, deltaTime, chunk);
+        glm::mat4 view = camera.getViewMatrix();
+
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        checkOpenGLError("set uniforms");
+
+        chunk.render(shaderProgram);
+        checkOpenGLError("render chunk");
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glfwTerminate();
+    return 0;
+}
